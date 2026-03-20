2db55a036e8c4afe44c36cf6bb320d61db34ca9d
Septdir <iberdichevskiy@septdir.com>
2026-03-20 15:35:25 +0300

    Cleanup

--- mod_radicalmart_search/tmpl/default.php
+++ mod_radicalmart_search/tmpl/default.php
@@ -11,10 +11,7 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\Registry\Registry;
 
