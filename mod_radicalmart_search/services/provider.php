2c37ba4d6210a059a5de56c1627f4d06ad5fef53
Septdir <iberdichevskiy@septdir.com>
2026-03-23 13:21:46 +0300

    Prepare 1.0.2 Release

--- mod_radicalmart_search/services/provider.php
+++ mod_radicalmart_search/services/provider.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Package
  * @subpackage  mod_radicalmart_analytics
- * @version     __DEPLOY_VERSION__
+ * @version     1.0.2
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2026 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
