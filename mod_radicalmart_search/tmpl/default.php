268802ce82083abdec75ad992501b1be36312532
Septdir <iberdichevskiy@septdir.com>
2026-03-30 12:51:38 +0300

    Reset to dev

--- mod_radicalmart_search/tmpl/default.php
+++ mod_radicalmart_search/tmpl/default.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Search Package
  * @subpackage  mod_radicalmart_search
- * @version     1.0.3
+ * @version     __DEPLOY_VERSION__
  * @author      RadicalMart Team - radicalmart.ru
  * @copyright   Copyright (c) 2026 RadicalMart. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
